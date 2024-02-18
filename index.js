0d644a4605bad6466609a1a1466a0fdf07d21f1b
Jamie Davies <jamiedavies@Jamies-MacBook-Air-4.local>
2024-02-18 22:28:58 +0000

    adding starting repository

---
+++ index.js
@@ -0,0 +1,29 @@
+var characters = [ 'moderator', 'werewolf', 'villager', 'villager', 'seer', 'doctor']
+var input = 
+
+
+if (input === 6 ){
+    return characters} 
+
+    switch(players) {
+        case 1:
+          // code block
+          break;
+        case 2:
+          // code block
+          break;
+        default:
+
+    
+
+
+
+
+
+//minimum number 
+/* do a range of 6-10
+case switch 
+ratio - for every 2 vilaggers 1 werewolf. + moderator.
+get list of how many characters there are
+look at tahe maths behind the game and decide whetheer to use ratio/ range/ case switch. 
+
